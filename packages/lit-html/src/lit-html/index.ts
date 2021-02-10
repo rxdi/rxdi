a5a2bd62cf534150ac051bb901c23f16ee119cbc
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2021-02-10 21:07:48 +0200

    feat(lit-html): added h utility function returning template result

--- packages/lit-html/src/lit-html/index.ts
+++ packages/lit-html/src/lit-html/index.ts
@@ -1,6 +1,12 @@
+import { html } from './lit-html';
+
 // export * from './directives/index';
 export * from './lit-html';
 export * from './directives/style-map';
 export * from './directives/rx';
 export * from './directives/class-map';
 export * from './directives/unsafe-html';
+
+
+export const h = <T>(v: T) => html`${v}`;
+
