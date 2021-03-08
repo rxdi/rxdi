e7df20a5d768d11939f192a66139b306b2deaf16
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2021-03-08 12:30:44 +0200

    fix(compressor): added correct types to compress and decompress

--- packages/compressor/src/lzw.ts
+++ packages/compressor/src/lzw.ts
@@ -3,14 +3,14 @@ import * as nodeLzw from "node-lzw";
 import * as safe64 from "urlsafe-base64";
 
 export class LZWService {
-  static compress(json: Record<string, unknown>) {
-    const packed = msgpack().encode(json);
+  static compress<T>(object: T): string {
+    const packed = msgpack().encode(object);
     const compressed = Buffer.from(nodeLzw.encode(packed.toString("binary")));
     const encoded = safe64.encode(compressed);
     return encoded;
   }
 
-  static decompress(string: string) {
+  static decompress<T>(string: string): T {
     const decoded = safe64.decode(string);
     const decompressed = Buffer.from(nodeLzw.decode(decoded), "binary");
     const unpacked = msgpack().decode(decompressed);
