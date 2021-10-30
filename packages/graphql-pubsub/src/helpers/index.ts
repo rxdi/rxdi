0e0099caf6653e61693c519eae2c22de35fcd14e
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2021-10-30 14:06:26 +0300

    fix(graphql-pubsub): barell export moved to client core

--- packages/graphql-pubsub/src/helpers/index.ts
+++ packages/graphql-pubsub/src/helpers/index.ts
@@ -1,4 +1,4 @@
-import { execute, DocumentNode } from '@apollo/client';
+import { execute, DocumentNode } from '@apollo/client/core';
 import { WebSocketLink } from '@apollo/client/link/ws';
 // import { gql } from 'apollo-server-core';
 import { ClientOptions } from 'subscriptions-transport-ws';
@@ -9,55 +9,55 @@ import { Observable } from 'rxjs';
 const ws = require('ws');
 
 export interface WSLinkOptions extends ClientOptions {
- server?: any;
- uri: string;
+    server?: any;
+    uri: string;
 }
 
 export interface ExtendedServer extends Server {
- port: number | string;
+    port: number | string;
 }
 
 export const createWebsocketLink = (
- options: WSLinkOptions = { uri: 'ws://localhost:9000/subscriptions' },
+    options: WSLinkOptions = { uri: 'ws://localhost:9000/subscriptions' },
 ) => {
- let server: ExtendedServer;
- try {
-  server = Container.get<ExtendedServer>(options.server || HAPI_SERVER);
- } catch (e) {}
- if (server) {
-  options.uri = `ws://localhost:${server.info.port || server.port}/subscriptions`;
- }
- if (!Container.has(WebSocketLink)) {
-  Container.set(
-   WebSocketLink,
-   new WebSocketLink({
-    uri: options.uri,
-    options: options || {
-     reconnect: true,
-    },
-    webSocketImpl: ws,
-   }),
-  );
- }
- return Container.get(WebSocketLink);
+    let server: ExtendedServer;
+    try {
+        server = Container.get<ExtendedServer>(options.server || HAPI_SERVER);
+    } catch (e) { }
+    if (server) {
+        options.uri = `ws://localhost:${server.info.port || server.port}/subscriptions`;
+    }
+    if (!Container.has(WebSocketLink)) {
+        Container.set(
+            WebSocketLink,
+            new WebSocketLink({
+                uri: options.uri,
+                options: options || {
+                    reconnect: true,
+                },
+                webSocketImpl: ws,
+            }),
+        );
+    }
+    return Container.get(WebSocketLink);
 };
 
 export const subscribeToTopic = <T, V = any>(
- query: DocumentNode,
- variables?: V,
- link?: WebSocketLink,
+    query: DocumentNode,
+    variables?: V,
+    link?: WebSocketLink,
 ): Observable<T> => {
- return new Observable<T>((o) => {
-  const cmd = execute(link || createWebsocketLink(), {
-   query,
-   variables,
-  }).subscribe({
-   next: o.next.bind(o),
-   error: o.error.bind(o),
-   complete: o.complete.bind(o),
-  });
-  return () => cmd.unsubscribe();
- });
+    return new Observable<T>((o) => {
+        const cmd = execute(link || createWebsocketLink(), {
+            query,
+            variables,
+        }).subscribe({
+            next: o.next.bind(o),
+            error: o.error.bind(o),
+            complete: o.complete.bind(o),
+        });
+        return () => cmd.unsubscribe();
+    });
 };
 
 export { WebSocketLink } from '@apollo/client/link/ws';
