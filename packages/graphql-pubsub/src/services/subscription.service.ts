740518608940be234b90f7b117ea977af68ee43a
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2025-12-23 20:33:21 +0200

    feat(ws): bump version of graphql-ws from subscription-ws

--- packages/graphql-pubsub/src/services/subscription.service.ts
+++ packages/graphql-pubsub/src/services/subscription.service.ts
@@ -1,75 +1,83 @@
-import {
-  Plugin,
-  Inject,
-  Service,
-  PluginInterface,
-  Container
-} from '@rxdi/core';
-import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { Inject, Service, PluginInterface, Container } from '@rxdi/core';
+import { useServer } from 'graphql-ws/use/ws';
+import { WebSocketServer } from 'ws'; // yarn add ws
+
 import { subscribe } from 'graphql/subscription';
 import { execute } from 'graphql/execution';
 import { HAPI_SERVER } from '@rxdi/hapi';
 import { Server } from '@hapi/hapi';
 import { GRAPHQL_PLUGIN_CONFIG } from '@rxdi/graphql';
 import {
-  GRAPHQL_PUB_SUB_CONFIG,
-  GRAPHQL_PUB_SUB_DI_CONFIG,
-  PubSubOptions
+ GRAPHQL_PUB_SUB_CONFIG,
+ GRAPHQL_PUB_SUB_DI_CONFIG,
+ PubSubOptions,
 } from '../config.tokens';
 
 @Service()
 export class SubscriptionService implements PluginInterface {
-  constructor(
-    @Inject(HAPI_SERVER) private server: Server,
-    @Inject(GRAPHQL_PLUGIN_CONFIG) private config: GRAPHQL_PLUGIN_CONFIG,
-    @Inject(GRAPHQL_PUB_SUB_CONFIG) private pubConfig: GRAPHQL_PUB_SUB_DI_CONFIG
-  ) { }
+ constructor(
+  @Inject(HAPI_SERVER) private server: Server,
+  @Inject(GRAPHQL_PLUGIN_CONFIG) private config: GRAPHQL_PLUGIN_CONFIG,
+  @Inject(GRAPHQL_PUB_SUB_CONFIG) private pubConfig: GRAPHQL_PUB_SUB_DI_CONFIG,
+ ) {}
 
-  OnInit() {
-    console.log('Subscription');
-    this.register();
+ OnInit() {
+  console.log('Subscription');
+  this.register();
+ }
+
+ async register() {
+  const currentC: any = {
+   execute,
+   subscribe,
+   schema: this.config.graphqlOptions.schema,
+   onConnect(connectionParams) {
+    // return connectionHookService.modifyHooks
+    //   .onSubConnection(connectionParams);
+    return connectionParams;
+   },
+   onOperation: (connectionParams, params, webSocket) => {
+    return params;
+    // return connectionHookService.modifyHooks
+    //   .onSubOperation(
+    //     connectionParams,
+    //     params,
+    //     webSocket
+    //   );
+   },
+  };
+  if (this.pubConfig.authentication) {
+   const auth: PubSubOptions = Container.get(this.pubConfig.authentication);
+   Object.assign(currentC, auth);
+   if (auth.onSubConnection) {
+    currentC.onConnect = auth.onSubConnection.bind(auth);
+   }
+   if (auth.onSubOperation) {
+    currentC.onOperation = auth.onSubOperation.bind(auth);
+   }
+   if (auth.onSubOperationComplete) {
+    currentC.onComplete = auth.onSubOperationComplete.bind(auth);
+   }
+   if (auth.onSubDisconnect) {
+    currentC.onDisconnect = auth.onSubDisconnect.bind(auth);
+   }
+   if (auth['onSubscribe']) {
+    currentC.onSubscribe = auth['onSubscribe'].bind(auth);
+   }
   }
 
-  async register() {
-    const currentC: any = {
-      execute,
-      subscribe,
-      schema: this.config.graphqlOptions.schema,
-      onConnect(connectionParams) {
-        // return connectionHookService.modifyHooks
-        //   .onSubConnection(connectionParams);
-        return connectionParams;
-      },
-      onOperation: (connectionParams, params, webSocket) => {
-        return params;
-        // return connectionHookService.modifyHooks
-        //   .onSubOperation(
-        //     connectionParams,
-        //     params,
-        //     webSocket
-        //   );
-      }
-    };
-    if (this.pubConfig.authentication) {
-      const auth: PubSubOptions = Container.get(this.pubConfig.authentication);
-      Object.assign(currentC, auth);
-      if (auth.onSubConnection) {
-        currentC.onConnect = auth.onSubConnection.bind(auth);
-      }
-      if (auth.onSubOperation) {
-        currentC.onOperation = auth.onSubOperation.bind(auth);
-      }
-      if (auth.onSubOperationComplete) {
-        currentC.onOperationComplete = auth.onSubOperationComplete.bind(auth);
-      }
-      if (auth.onSubDisconnect) {
-        currentC.onDisconnect = auth.onSubDisconnect.bind(auth);
-      }
-    }
-    new SubscriptionServer(currentC, {
-      server: this.server.listener,
-      path: '/subscriptions',
-      ...this.pubConfig.subscriptionServerOptions,
-    });
-  }
+  const server = new WebSocketServer({
+   path: '/subscriptions',
+   server: this.server.listener,
+   ...this.pubConfig.subscriptionServerOptions,
+  });
+
+  useServer(
+   {
+    ...currentC,
+    schema: this.config.graphqlOptions.schema,
+   },
+   server,
+  );
+ }
 }
